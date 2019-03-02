35dfb50bc413f3d28e39b89af228f318839ebd44
Kenny <khpham24@gmail.com>
2019-03-02 12:54:20 -0700

    updated packages to fix vulnerabilies/ removed contact form

--- src/components/fullpage/fullpage.js
+++ src/components/fullpage/fullpage.js
@@ -58,21 +58,7 @@ class Fullpage extends React.Component {
               </div>
               <div className="section">
                 <h2>Contact</h2>
-                <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+                
               </div>
             </ReactFullpage.Wrapper>
           );
